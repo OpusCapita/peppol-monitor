e8c5fe5e170903856da8733275c99569bf387b21
thebilge <ibrahimbilge7@gmail.com>
2019-05-13 14:19:19 +0200

    some styling for validator

--- src/main/client/components/StandaloneValidator/StandaloneValidator.react.js
+++ src/main/client/components/StandaloneValidator/StandaloneValidator.react.js
@@ -10,7 +10,6 @@ class StandaloneValidator extends Components.ContextComponent {
     state = {
         loading: false,
         result: {},
-        showResult: false,
         showInfos: true,
         showErrors: true,
         showWarnings: true,
@@ -28,12 +27,12 @@ class StandaloneValidator extends Components.ContextComponent {
             return;
         }
 
-        this.setState({loading: true, showResult: false});
+        this.setState({loading: true});
         let data = new FormData();
         data.append('file', file);
 
         this.api.validateFile(data).then((response) => {
-            this.setState({loading: false, showResult: true, result: response});
+            this.setState({loading: false, result: response});
         }).catch(e => {
             this.setState({loading: false});
             this.context.showNotification(e.message, 'error', 10);
@@ -43,8 +42,12 @@ class StandaloneValidator extends Components.ContextComponent {
     getMessages() {
         const {result, showInfos, showErrors, showWarnings} = this.state;
 
-        if (!result || !result.messages) {
-            return [];
+        if (!result) {
+            return []
+        }
+
+        if (!result.messages) {
+            return [{time: 1, level: "INFO", message: "Validation SUCCESSFUL!"}];
         }
 
         return result.messages.filter(log => {
@@ -75,7 +78,7 @@ class StandaloneValidator extends Components.ContextComponent {
     }
 
     render() {
-        const {loading, result, showResult, showInfos, showErrors, showWarnings} = this.state;
+        const {loading, result, showInfos, showErrors, showWarnings} = this.state;
 
         return (
             <div>
@@ -84,67 +87,74 @@ class StandaloneValidator extends Components.ContextComponent {
                     Select the document for validation!
                     <input type="file" hidden onChange={e => this.validateFile(e)}/>
                 </label>
-                {
-                    showResult &&
-                    <div>
-                        <h3>Validation Result</h3>
-                        <div className="form-horizontal transmission-detail">
-                            <div className="row">
-                                <div className="col-md-10 col-md-offset-1">
-                                    <div className="doc-type-label">({result.rule.id}) {result.rule.description}</div>
-                                </div>
-                            </div>
-                            <div className="row">
-                                <div className="col-md-11">
-                                    <ReactTable
-                                        className="message-detail-history-table"
-                                        data={this.getMessages()}
-                                        loading={loading}
-                                        columns={[
-                                            {
-                                                id: 'type',
-                                                width: 110,
-                                                Header: 'Type',
-                                                accessor: log => log,
-                                                Cell: ({value}) =>
-                                                    <span>
-                                                        <span className={`label label-${this.getTypeLabelClass(value.level)}`}>
-                                                            {(value.level === 'ERROR') ? value.errorType : value.level}
-                                                        </span>
-                                                    </span>
-                                            },
-                                            {
-                                                id: 'message',
-                                                Header: 'Message',
-                                                accessor: 'message',
-                                            }
-                                        ]}
-                                        sorted={[{
-                                            id: 'time',
-                                            desc: true
-                                        }]}
-                                        minRows={5}
-                                        defaultPageSize={100}
-                                    />
-                                </div>
+                <div>
+                    <h3>Validation Result</h3>
+                    <div className="form-horizontal transmission-detail">
+                        <div className="row">
+                            <div className="col-md-10 col-md-offset-1">
+                                <div className="doc-type-label">({result.rule.id}) {result.rule.description}</div>
                             </div>
                         </div>
-                        <div className="form-submit text-right transmission-detail-actions">
-                            { showInfos
-                                ? <button className="btn btn-info" onClick={() => this.setState({showInfos: false})}>Hide Infos</button>
-                                : <button className="btn btn-info btn-passive" onClick={() => this.setState({showInfos: true})}>Show Infos</button>
-                            }
-                            { showWarnings
-                                ? <button className="btn btn-warning" onClick={() => this.setState({showWarnings: false})}>Hide Warnings</button>
-                                : <button className="btn btn-warning btn-passive" onClick={() => this.setState({showWarnings: true})}>Show Warnings</button>
-                            }
-                            { showErrors
-                                ? <button className="btn btn-danger" onClick={() => this.setState({showErrors: false})}>Hide Errors</button>
-                                : <button className="btn btn-danger btn-passive" onClick={() => this.setState({showErrors: true})}>Show Errors</button>
-                            }
+                        <div className="row">
+                            <div className="col-md-12">
+                                <ReactTable
+                                    className="message-detail-history-table"
+                                    data={this.getMessages()}
+                                    loading={loading}
+                                    columns={[
+                                        {
+                                            id: 'type',
+                                            width: 110,
+                                            Header: 'Type',
+                                            accessor: log => log,
+                                            Cell: ({value}) =>
+                                                <span>
+                                                    <span className={`label label-${this.getTypeLabelClass(value.level)}`}>
+                                                        {value.level}
+                                                    </span>
+                                                </span>
+                                        },
+                                        {
+                                            id: 'code',
+                                            width: 100,
+                                            Header: 'Code',
+                                            accessor: log => log,
+                                            Cell: ({value}) =>
+                                                <span>
+                                                    {(value.validationError && value.validationError.identifier) ? value.validationError.identifier : 'N/A'}
+                                                </span>
+                                        },
+                                        {
+                                            id: 'message',
+                                            Header: 'Message',
+                                            accessor: 'message',
+                                        }
+                                    ]}
+                                    sorted={[{
+                                        id: 'time',
+                                        desc: true
+                                    }]}
+                                    minRows={5}
+                                    defaultPageSize={100}
+                                />
+                            </div>
                         </div>
                     </div>
-                }
+                    <div className="form-submit text-right transmission-detail-actions">
+                        { showInfos
+                            ? <button className="btn btn-info" onClick={() => this.setState({showInfos: false})}>Hide Infos</button>
+                            : <button className="btn btn-info btn-passive" onClick={() => this.setState({showInfos: true})}>Show Infos</button>
+                        }
+                        { showWarnings
+                            ? <button className="btn btn-warning" onClick={() => this.setState({showWarnings: false})}>Hide Warnings</button>
+                            : <button className="btn btn-warning btn-passive" onClick={() => this.setState({showWarnings: true})}>Show Warnings</button>
+                        }
+                        { showErrors
+                            ? <button className="btn btn-danger" onClick={() => this.setState({showErrors: false})}>Hide Errors</button>
+                            : <button className="btn btn-danger btn-passive" onClick={() => this.setState({showErrors: true})}>Show Errors</button>
+                        }
+                    </div>
+                </div>
             </div>
         );
     }
