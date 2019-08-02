4cbe59f58d7b186ddf49c0701543fe7a273414ce
thebilge <ibrahimbilge7@gmail.com>
2019-08-02 11:07:43 +0200

    workaround for couldn't extract result set exception

--- src/main/java/com/opuscapita/peppol/monitor/repository/MessageService.java
+++ src/main/java/com/opuscapita/peppol/monitor/repository/MessageService.java
@@ -1,15 +1,19 @@
 package com.opuscapita.peppol.monitor.repository;
 
 import com.opuscapita.peppol.monitor.entity.Message;
+import org.springframework.retry.annotation.Backoff;
+import org.springframework.retry.annotation.Retryable;
 
 import java.util.List;
 
 public interface MessageService {
 
+    @Retryable(value = {Exception.class}, maxAttempts = 3, backoff = @Backoff(delay = 1000))
     Message saveMessage(Message message);
 
     Message getMessage(Long id);
 
+    @Retryable(value = {Exception.class}, maxAttempts = 3, backoff = @Backoff(delay = 1000))
     Message getMessage(String messageId);
 
     void deleteMessage(Message message);
