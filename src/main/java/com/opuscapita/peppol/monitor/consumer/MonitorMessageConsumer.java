cf1127e30e72eda6565e2735ff55abd1ebea9fbd
wilhelm71 <wilhelm71@users.noreply.github.com>
2021-03-31 09:19:50 +0200

    Force commit to deploy to stage (added space)

--- src/main/java/com/opuscapita/peppol/monitor/consumer/MonitorMessageConsumer.java
+++ src/main/java/com/opuscapita/peppol/monitor/consumer/MonitorMessageConsumer.java
@@ -20,7 +20,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-import java.util.stream.Collectors;
+import java.util.stream.Collectors; 
 
 @Component
 public class MonitorMessageConsumer implements ContainerMessageConsumer {
