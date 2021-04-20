474465147d87bbd7ce8491af19e57a627d6387c6
Dusty Wilhelm Murray <wilhelm71@users.noreply.github.com>
2021-04-20 15:56:15 +0200

    Update TransmissionTable.react.js

--- src/main/client/components/TransmissionTable/TransmissionTable.react.js
+++ src/main/client/components/TransmissionTable/TransmissionTable.react.js
@@ -437,7 +437,7 @@ class TransmissionTable extends Components.ContextComponent {
                                 </div>
                                 <div className="form-group">
                                     <div className="col-sm-3">
-                                        <label className="control-label">Invoice Number</label>
+                                        <label className="control-label">Document Number</label>
                                     </div>
                                     <div className="offset-md-1 col-md-8">
                                         <input type="text" className="form-control" value={searchValues.invoiceNumber}
